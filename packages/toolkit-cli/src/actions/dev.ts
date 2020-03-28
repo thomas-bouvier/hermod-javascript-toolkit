5169bc20b226ef9c200c5c2c5cfb200ceb912dae
Thomas Bouvier <contact@thomas-bouvier.io>
2020-03-28 01:08:42 +0100

    Change dev action return type to a promise

--- packages/toolkit-cli/src/actions/dev.ts
+++ packages/toolkit-cli/src/actions/dev.ts
@@ -7,7 +7,7 @@ import chalk from 'chalk'
 const defaultEntry = path.join(process.cwd(), 'src', 'index')
 const defaultOutput = path.join(process.cwd(), 'dist')
 
-export function dev ({ entry = defaultEntry, output = defaultOutput, configPath, sandbox }) {
+export async function dev ({ entry = defaultEntry, output = defaultOutput, configPath, sandbox }) {
     console.log(chalk.bold('> Running in dev mode…\n'))
 
     const webpackConfig = require('../../config/webpack.dev.js')({
@@ -17,10 +17,24 @@ export function dev ({ entry = defaultEntry, output = defaultOutput, configPath,
         sandbox
     })
 
-    return webpack(webpackConfig, (_, stats) => {
-        console.log(stats.toString({
-            chunks: false,
-            colors: true
-        }) + '\n')
+    return new Promise((resolve, reject) => {
+        webpack(webpackConfig, (err, stats) => {
+            if (err) {
+                console.error(chalk.red(err.stack || err.toString()))
+                if (err['details']) {
+                  console.error(chalk.red(err['details']))
+                }
+                reject(err)
+            }
+
+            console.log(stats.toString({
+                chunks: false,
+                colors: true
+            }) + '\n')
+
+            resolve()
+        })
     })
+    .then(() => console.log(chalk.bold.green('\n> Dev mode started!')))
+    .catch(error => error && error.message && console.error(chalk.bold.red('\n> Dev mode failed…\n\n', error)))
 }
